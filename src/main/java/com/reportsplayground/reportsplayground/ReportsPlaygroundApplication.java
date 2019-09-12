f53b97e9ec252fc5dd6369b0f4658fcb0cee9005
jorge <jorgehasd@gmail.com>
2019-09-12 11:44:02 -0300

    initial commit

---
+++ src/main/java/com/reportsplayground/reportsplayground/ReportsPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.reportsplayground.reportsplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportsPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportsPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reportsplayground/reportsplayground/ReportsPlaygroundApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reportsplayground.reportsplayground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReportsPlaygroundApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
